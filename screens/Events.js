143fe31d4cffc4e5dc450e5e67e4532fb3155da9
Mike Chambers <MikeChambers610@gmail.com>
2024-05-02 13:51:47 -0400

    trying deep link in new events folder again

--- screens/Events.js
+++ screens/Events.js
@@ -5,6 +5,7 @@ const Events = () => {
   const [loading, setLoading] = useState(true); // Start with loading true
 
   useEffect(() => {
+    //new build
     const xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://q0ekxtr8l3.execute-api.us-east-1.amazonaws.com/v1/events?groupID=87ea0c82-0d15-4e4b-b8d9-ab0daf2fcb3f&sortID=2023-06-22T22:02:16.063Z4cbfaaa8-7cbc-4b87-bb9f-c0fc17e94540');
 
